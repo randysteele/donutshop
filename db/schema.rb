bebf2b9717230c7b12e112113874863a057d86ab
steelerx155 <steelerx155@gmail.com>
2021-03-02 10:07:50 -0500

    created devise for user and added registrations controller with sign_up_params and account_update_params

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_010337) do
+ActiveRecord::Schema.define(version: 2021_03_02_145449) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 2021_03_02_010337) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "line_items", "carts"
