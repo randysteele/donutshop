f7192d84299febc53a252d81460ff507e1822b33
steelerx155 <steelerx155@gmail.com>
2021-03-02 10:32:11 -0500

    fixed syntax error in registrations controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_145449) do
+ActiveRecord::Schema.define(version: 2021_03_02_152608) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_145449) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
